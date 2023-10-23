225288e6ed560c2d81fd34931f107d26b81fcfdf
Ryo <107451261+ryotahidaka@users.noreply.github.com>
2023-10-23 14:04:25 -0400

    First commit for signup interface_adapter

--- src/interface_adapter/signup/SignupPresenter.java
+++ src/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,42 @@
 package interface_adapter.signup;
 
-public class SignupPresenter {
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.signup.SignupOutputBoundary;
+import use_case.signup.SignupOutputData;
+
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final SignupViewModel signupViewModel;
+    private final LoginViewModel loginViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel,
+                           LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData signupOutputData) {
+        // If sign up is successful, switch to the login view.
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(signupOutputData.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        // If sign up is not successful, remain on page and send out an error.
+        SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(error);
+        signupViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapter/signup/SignupState.java
+++ src/interface_adapter/signup/SignupState.java
@@ -1,4 +1,83 @@
 package interface_adapter.signup;
 
 public class SignupState {
+    private String name = "";
+    private String nameError = null;
+    private String username = "";
+    private String usernameError = null;
+    private String password = "";
+    private String passwordError = null;
+    private String repeatPassword = "";
+    private String repeatPasswordError = null;
+
+    public SignupState(SignupState copy) {
+        name = copy.name;
+        nameError = copy.nameError;
+        username = copy.username;
+        usernameError = copy.usernameError;
+        password = copy.password;
+        passwordError = copy.passwordError;
+        repeatPassword = copy.repeatPassword;
+        repeatPasswordError = copy.repeatPasswordError;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public SignupState() {
+    }
+
+    public String getName() { return name; }
+    public String getNameError() { return nameError; }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getUsernameError() { return usernameError; }
+
+    public String getPassword() {
+        return password;
+    }
+    public String getPasswordError() {
+        return passwordError;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+    public String getRepeatPasswordError() {
+        return repeatPasswordError;
+    }
+
+    public void setName(String name) { this.name = name; }
+    public void setNameError(String nameError) { this.nameError = nameError; }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setUsernameError(String usernameError) {
+        this.usernameError = usernameError;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setPasswordError(String passwordError) {
+        this.passwordError = passwordError;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+    public void setRepeatPasswordError(String repeatPasswordError) {
+        this.repeatPasswordError = repeatPasswordError;
+    }
+
+    @Override
+    public String toString() {
+        return "SignupState{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                '}';
+    }
 }
