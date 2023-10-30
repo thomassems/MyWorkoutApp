736869652cc4cbff33c0b01cd686ab726b0655e0
catou88 <catherinezheng25@gmail.com>
2023-10-30 13:33:30 -0400

    did search controller

--- src/interface_adapter/search/SearchController.java
+++ src/interface_adapter/search/SearchController.java
@@ -1,4 +1,17 @@
 package interface_adapter.search;
 
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInputData;
+
 public class SearchController {
+    final SearchInputBoundary searchUseCaseInteractor;
+    public SearchController(SearchInputBoundary searchUseCaseInteractor) {
+        this.searchUseCaseInteractor = searchUseCaseInteractor;
+    }
+
+    public void execute(String name, String muscle, String difficulty, String description) {
+        SignupInputData signupInputData = new SignupInputData(name, muscle, difficulty, description);
+        searchUseCaseInteractor.execute(signupInputData);
+    }
 }
+
