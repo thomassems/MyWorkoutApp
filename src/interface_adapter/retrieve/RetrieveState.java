9b15b11a2833781b6f678e9aa26cd9c1aea8709c
catou88 <catherinezheng25@gmail.com>
2023-11-03 14:22:35 -0400

    retrieve state

--- src/interface_adapter/retrieve/RetrieveState.java
+++ src/interface_adapter/retrieve/RetrieveState.java
@@ -1,4 +1,27 @@
 package interface_adapter.retrieve;
 
+import interface_adapter.login.LoginState;
+
+import java.util.ArrayList;
+
 public class RetrieveState {
+    // Initialize private member variables for username and password fields
+    private ArrayList<ArrayList<String>> exercises = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<String>> exercisesError = null;
+
+    // Copy constructor for creating a new LoginState instance as a copy of another LoginState
+    public RetrieveState(RetrieveState copy) {
+        // Copy the values of the username, usernameError, password, and passwordError from the provided 'copy' instance
+        exercises = copy.exercises;
+        exercisesError = copy.exercisesError;
+    }
+    // Default constructor to create an empty LoginState (required due to the presence of a copy constructor)
+    public RetrieveState() {}
+    // Getter methods to retrieve the values of username, usernameError, password, and passwordError
+    public ArrayList<ArrayList<String>> getExercises() {
+        return exercises;
+    }
+    public ArrayList<ArrayList<String>> getExercisesErrorError() {
+        return exercisesError;
+    }
 }
