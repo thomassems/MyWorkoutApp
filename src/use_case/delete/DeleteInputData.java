ba84af0fb1ae9227ee57d4d8818a36b5bb89fa91
Miguel Garcia Alonso <miguel.garciaalonso@mail.utoronto.ca>
2023-11-03 06:33:30 +0100

    Delete implementation draft

--- src/use_case/delete/DeleteInputData.java
+++ src/use_case/delete/DeleteInputData.java
@@ -1,4 +1,12 @@
 package use_case.delete;
 
 public class DeleteInputData {
+    final private String username;
+    public DeleteInputData(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
--- src/use_case/delete/DeleteInteractor.java
+++ src/use_case/delete/DeleteInteractor.java
@@ -1,4 +1,16 @@
 package use_case.delete;
 
-public class DeleteInteractor {
+public class DeleteInteractor implements DeleteInputBoundary{
+    final DeleteUserDataAccessInterface userDataAccessObject;
+    final DeleteOutputBoundary deletePresenter;
+    public DeleteInteractor(DeleteUserDataAccessInterface userDataAccessInterface, DeleteOutputBoundary deleteOutputBoundary){
+        this.userDataAccessObject = userDataAccessInterface;
+        this.deletePresenter = deleteOutputBoundary;
+    }
+    @Override
+    public void execute(DeleteInputData deleteInputData){
+        userDataAccessObject.delete(deleteInputData.getUsername());
+        DeleteOutputData deleteOutputData = new DeleteOutputData();
+        deletePresenter.prepareSuccessViews(deleteOutputData);
+    }
 }
