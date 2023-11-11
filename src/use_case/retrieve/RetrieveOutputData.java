26cd1f6aef4effd2e8bd68a6c08328304b595377
catou88 <catherinezheng25@gmail.com>
2023-11-11 17:42:44 -0500

    completed output data file for retrieve use case

--- src/use_case/retrieve/RetrieveOutputData.java
+++ src/use_case/retrieve/RetrieveOutputData.java
@@ -1,4 +1,17 @@
 package use_case.retrieve;
 
+import entity.Exercise;
+
+import java.util.ArrayList;
+
 public class RetrieveOutputData {
+    final private ArrayList<Exercise> exercises;
+
+    public RetrieveOutputData(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
 }
