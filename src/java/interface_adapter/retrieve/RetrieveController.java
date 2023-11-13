ae14f01336ac5d51e2464fb3374f119d1be49185
catou88 <100166405+catou88@users.noreply.github.com>
2023-11-13 14:41:31 -0500

    Update RetrieveController.java

--- src/java/interface_adapter/retrieve/RetrieveController.java
+++ src/java/interface_adapter/retrieve/RetrieveController.java
@@ -1,4 +1,16 @@
 package interface_adapter.retrieve;
 
+import use_case.retrieve.RetrieveInputBoundary;
+import use_case.retrieve.RetrieveInputData;
+
 public class RetrieveController {
+    final RetrieveInputBoundary retrieveUseCaseInteractor;
+    public RetrieveController(RetrieveInputBoundary retrieveUseCaseInteractor) {
+        this.retrieveUseCaseInteractor = retrieveUseCaseInteractor;
+    }
+
+    public void execute(String username) {
+        RetrieveInputData retrieveInputData = new RetrieveInputData(username);
+        retrieveUseCaseInteractor.execute(retrieveInputData);
+    }
 }
