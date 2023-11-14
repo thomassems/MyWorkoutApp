9e5d6b45cb2db2aab32ab4b90064150deb36fbd3
Ryo <107451261+ryotahidaka@users.noreply.github.com>
2023-11-14 09:38:23 -0500

    Fixed ResultsState getExercise and other namings

--- src/java/interface_adapter/results/ResultsState.java
+++ src/java/interface_adapter/results/ResultsState.java
@@ -7,33 +7,33 @@ import java.util.ArrayList;
 public class ResultsState {
 
     // Initialize private member variable for search and list of exercise results
-    private ArrayList<ArrayList<String>> exerciseSearchResults = new ArrayList<ArrayList<String>>();
-    private ArrayList<ArrayList<String>> exerciseSearchResultsError = null;
+    private ArrayList<ArrayList<String>> exercise = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<String>> exerciseError = null;
 
     // Copy constructor for creating a new LoginState instance as a copy of another LoginState
     public ResultsState(ResultsState copy) {
         // Copy the values from the provided 'copy' instance
-        exerciseSearchResults = copy.exerciseSearchResults;
-        exerciseSearchResultsError = copy.exerciseSearchResultsError;
+        exercise = copy.exercise;
+        exerciseError = copy.exerciseError;
     }
     // Default constructor to create an empty LoginState (required due to the presence of a copy constructor)
     public ResultsState() {}
     // Getter methods to retrieve the values of exerciseSearchResults
 
-    public ArrayList<ArrayList<String>> getSearchExerciseResults() {
-        return exerciseSearchResults;
+    public ArrayList<ArrayList<String>> getExercise() {
+        return exercise;
     }
 
-    public ArrayList<ArrayList<String>> getExerciseSearchResultsError() {
-        return exerciseSearchResultsError;
+    public ArrayList<ArrayList<String>> getExerciseError() {
+        return exerciseError;
     }
 
     // Setter methods to set the values of exerciseSearchResults
 
-    public void setExerciseSearchResults(ArrayList<ArrayList<String>> searchExerciseResults) {
-        this.exerciseSearchResults = searchExerciseResults;
+    public void setExercise(ArrayList<ArrayList<String>> exercise) {
+        this.exercise = exercise;
     }
-    public void setExerciseSearchResultsError(ArrayList<ArrayList<String>> searchExerciseResultsError) {
-        this.exerciseSearchResultsError = searchExerciseResultsError;
+    public void setExerciseError(ArrayList<ArrayList<String>> exerciseError) {
+        this.exerciseError = exerciseError;
     }
 }
